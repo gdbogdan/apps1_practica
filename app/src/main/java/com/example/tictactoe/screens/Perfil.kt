f29f7a82d3d9eecb8819a1d9aac8876aa48a6921
Gabriel <gb9@alumnes.udl.cat>
2025-04-24 20:07:40 +0200

    Perfil mejorado para landscape y portrait

--- app/src/main/java/com/example/tictactoe/screens/Perfil.kt
+++ app/src/main/java/com/example/tictactoe/screens/Perfil.kt
@@ -1,12 +1,15 @@
 package com.example.tictactoe.screens
 
 import android.annotation.SuppressLint
+import android.content.res.Configuration
 import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
@@ -16,25 +19,43 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.tictactoe.R
 import com.example.tictactoe.view_models.PerfilViewModel
-import java.util.concurrent.TimeUnit
 
 @SuppressLint("DefaultLocale")
 @Composable
 fun Perfil(
+    navController: NavController,
+    perfilViewModel: PerfilViewModel
+) {
+    val configuration = LocalConfiguration.current
+    when (configuration.orientation) {
+        Configuration.ORIENTATION_PORTRAIT -> {
+            PerfilPortrait(navController, perfilViewModel)
+        }
+        Configuration.ORIENTATION_LANDSCAPE -> {
+            PerfilLandscape(navController, perfilViewModel)
+        }
+        else -> {
+            PerfilPortrait(navController, perfilViewModel)
+        }
+    }
+}
+
+@SuppressLint("DefaultLocale")
+@Composable
+fun PerfilPortrait(
     navController: NavController,
     perfilViewModel: PerfilViewModel
 ) {
@@ -49,8 +70,11 @@ fun Perfil(
     val minutos by perfilViewModel.minutos
     val segundos by perfilViewModel.segundos
 
-
-    Column(modifier = Modifier.padding(16.dp).verticalScroll(scrollState)) {
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .verticalScroll(scrollState)
+    ) {
         // Alias
         OutlinedTextField(
             value = alias,
@@ -63,9 +87,9 @@ fun Perfil(
 
         Spacer(modifier = Modifier.padding(8.dp))
 
-        // Dificultad: Difícil -> true, Fácil -> false
+        // Dificultad
         Text(text = stringResource(R.string.dificultad))
-        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(16.dp)) {
+        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(vertical = 8.dp)) {
             RadioButton(
                 selected = !dificultad,
                 onClick = { if (isEditing) perfilViewModel.actualizarDificultad(false) },
@@ -83,9 +107,9 @@ fun Perfil(
 
         Spacer(modifier = Modifier.padding(8.dp))
 
-        // Temporizador: Si -> true, No -> false
+        // Temporizador
         Text(text = stringResource(R.string.temporizador))
-        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(16.dp)) {
+        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(vertical = 8.dp)) {
             RadioButton(
                 selected = !temporizador,
                 onClick = { if (isEditing) perfilViewModel.actualizarTemporizador(false) },
@@ -101,7 +125,7 @@ fun Perfil(
             Text(text = stringResource(R.string.si))
         }
 
-        // Configuración del Tiempo Máximo (solo visible si se está editando y temporizador == true)
+        // Configuración del Tiempo Máximo
         if (temporizador) {
             Text(text = stringResource(R.string.tiempo_maximo))
             Row(verticalAlignment = Alignment.CenterVertically) {
@@ -115,7 +139,7 @@ fun Perfil(
                     placeholder = { Text(stringResource(R.string.zero)) },
                     modifier = Modifier.weight(1f),
                     singleLine = true,
-                    enabled = isEditing // Solo editable si isEditing es true
+                    enabled = isEditing
                 )
 
                 Text(text = ":", modifier = Modifier.padding(horizontal = 8.dp))
@@ -130,7 +154,7 @@ fun Perfil(
                     placeholder = { Text(stringResource(R.string.zero)) },
                     modifier = Modifier.weight(1f),
                     singleLine = true,
-                    enabled = isEditing // Solo editable si isEditing es true
+                    enabled = isEditing
                 )
             }
         }
@@ -142,10 +166,8 @@ fun Perfil(
             horizontalArrangement = Arrangement.Center
         ) {
             if (isEditing) {
-                // Modo Edición
                 Button(
                     onClick = {
-                        // Guardar otros valores y salir del modo edición
                         Toast.makeText(context, toastMsg, Toast.LENGTH_SHORT).show()
                         perfilViewModel.marcarPrimerJuegoComoJugado()
                         perfilViewModel.actualizarAlias(alias)
@@ -161,7 +183,6 @@ fun Perfil(
 
                 Button(
                     onClick = {
-                        // Cancelar los cambios
                         perfilViewModel.restablecerValoresOriginales()
                     }
                 ) {
@@ -191,4 +212,197 @@ fun Perfil(
             }
         }
     }
+}
+
+@SuppressLint("DefaultLocale")
+@Composable
+fun PerfilLandscape(
+    navController: NavController,
+    perfilViewModel: PerfilViewModel
+) {
+    val context = LocalContext.current
+
+    val isEditing by perfilViewModel.isEditing
+    val alias by perfilViewModel.alias
+    val dificultad by perfilViewModel.dificultad
+    val temporizador by perfilViewModel.temporizador
+    val minutos by perfilViewModel.minutos
+    val segundos by perfilViewModel.segundos
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp),
+        horizontalAlignment = Alignment.Start
+    ) {
+        // Fila 1: Alias, Dificultad y Temporizador (INTACTA)
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            OutlinedTextField(
+                value = alias,
+                onValueChange = { if (isEditing) perfilViewModel.actualizarAlias(it) },
+                enabled = isEditing,
+                placeholder = { Text(stringResource(R.string.placeholder)) },
+                modifier = Modifier.weight(1f),
+                label = { Text(stringResource(R.string.alias)) }
+            )
+
+            Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.weight(0.5f)) {
+                Text(text = stringResource(R.string.dificultad))
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    RadioButton(
+                        selected = !dificultad,
+                        onClick = { if (isEditing) perfilViewModel.actualizarDificultad(false) },
+                        enabled = isEditing
+                    )
+                    Text(text = stringResource(R.string.facil))
+                    Spacer(modifier = Modifier.width(8.dp))
+                    RadioButton(
+                        selected = dificultad,
+                        onClick = { if (isEditing) perfilViewModel.actualizarDificultad(true) },
+                        enabled = isEditing
+                    )
+                    Text(text = stringResource(R.string.dificil))
+                }
+            }
+
+            Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.weight(0.6f)) {
+                Text(text = stringResource(R.string.temporizador))
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    RadioButton(
+                        selected = !temporizador,
+                        onClick = { if (isEditing) perfilViewModel.actualizarTemporizador(false) },
+                        enabled = isEditing
+                    )
+                    Text(text = stringResource(R.string.no))
+                    Spacer(modifier = Modifier.width(8.dp))
+                    RadioButton(
+                        selected = temporizador,
+                        onClick = { if (isEditing) perfilViewModel.actualizarTemporizador(true) },
+                        enabled = isEditing
+                    )
+                    Text(stringResource(R.string.si))
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Fila 2: Minutos/Segundos (si temporizador) y Botones
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically // Alineación vertical centrada
+        ) {
+            // Contenedor para Minutos y Segundos
+            if (temporizador) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.weight(0.5f)) {
+                    Text(text = stringResource(R.string.tiempo_maximo), modifier = Modifier.padding(end = 8.dp))
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        OutlinedTextField(
+                            value = String.format("%02d", minutos),
+                            onValueChange = { nuevoTexto ->
+                                val valor = nuevoTexto.toIntOrNull() ?: 0
+                                perfilViewModel.actualizarMinutos(valor)
+                            },
+                            label = { Text(stringResource(R.string.minutos)) },
+                            placeholder = { Text(stringResource(R.string.zero)) },
+                            modifier = Modifier.weight(1f),
+                            singleLine = true,
+                            enabled = isEditing
+                        )
+
+                        Text(text = ":", modifier = Modifier.padding(horizontal = 8.dp))
+
+                        OutlinedTextField(
+                            value = String.format("%02d", segundos),
+                            onValueChange = { nuevoTexto ->
+                                val valor = nuevoTexto.toIntOrNull() ?: 0
+                                perfilViewModel.actualizarSegundos(valor)
+                            },
+                            label = { Text(stringResource(R.string.segundos)) },
+                            placeholder = { Text(stringResource(R.string.zero)) },
+                            modifier = Modifier.weight(1f),
+                            singleLine = true,
+                            enabled = isEditing
+                        )
+                    }
+                }
+            } else {
+                Column(modifier = Modifier.weight(0.5f)) {
+                    Spacer(modifier = Modifier.height(0.dp)) // Espacio si no hay temporizador
+                }
+            }
+
+            // Contenedor para los Botones
+            Row(
+                horizontalArrangement = Arrangement.SpaceAround,
+                modifier = Modifier.weight(0.5f)
+            ) {
+                if (!isEditing) {
+                    Button(
+                        onClick = {
+                            perfilViewModel.guardarValoresOriginales(
+                                alias,
+                                dificultad,
+                                temporizador,
+                                minutos,
+                                segundos
+                            )
+                            perfilViewModel.setEditing(true)
+                        }
+                    ) {
+                        Text(stringResource(R.string.editar))
+                    }
+                    Button(
+                        onClick = { navController.navigate("Inicio") }
+                    ) {
+                        Text(stringResource(R.string.inicio))
+                    }
+                } else {
+                    Button(
+                        onClick = {
+                            perfilViewModel.marcarPrimerJuegoComoJugado()
+                            perfilViewModel.actualizarAlias(alias)
+                            perfilViewModel.actualizarDificultad(dificultad)
+                            perfilViewModel.actualizarTemporizador(temporizador)
+                            perfilViewModel.actualizarMinutos(minutos)
+                            perfilViewModel.actualizarSegundos(segundos)
+                            perfilViewModel.setEditing(false)
+                        }
+                    ) {
+                        Text(stringResource(R.string.guardar))
+                    }
+                    Button(
+                        onClick = {
+                            perfilViewModel.restablecerValoresOriginales()
+                        }
+                    ) {
+                        Text(stringResource(R.string.cancelar))
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, device = "id:pixel_6")
+@Composable
+fun PerfilPortraitPreview() {
+    val navController = rememberNavController()
+    val viewModel: PerfilViewModel = viewModel()
+    PerfilPortrait(navController = navController, perfilViewModel = viewModel)
+}
+
+@Preview(showBackground = true, device = "spec:parent=pixel_6,orientation=landscape")
+@Composable
+fun PerfilLandscapePreview() {
+    val navController = rememberNavController()
+    val viewModel: PerfilViewModel = viewModel()
+    PerfilLandscape(navController = navController, perfilViewModel = viewModel)
 }
